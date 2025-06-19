88a9cbdbabbd94304a2f85106682397d19b66f1b
roldebijvank <r.olde.bijvank@student.tue.nl>
2025-06-19 18:32:49 +0200

    trying to get schedule to work

--- app.py
+++ app.py
@@ -1318,7 +1318,7 @@ def download_schedule(n_clicks, selected_ward):
         return dcc.send_data_frame(ward_df.to_csv, filename, index=False)
     
 def map_wards_function(selected_ward):
-    return ward_code[ward_code['GSS_Code'] == selected_ward]['Name'].values[0]
+    return ward_gdf[ward_gdf['GSS_Code'] == selected_ward]['Name'].values[0]
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
